4bfb2a880e54bd559d17058c7db31378be2059db
Austin <austin.cotant@gmail.com>
2018-09-29 14:15:15 -0700

    Add react

---
+++ src/js/chess.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+$(() => {
+
+  ReactDom.render(
+    <h1>Go React!</h1>,
+      document.getElementById('root')
+  );
+
+});
--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,11 @@
 const View = require('./view.js');
 const Game = require('./game.js');
 
-$(() => {
+const renderView = () => {
   const $mainContainer = $('.board-container');
   const game = new Game();
   const view = new View(game, $mainContainer);
-  while (false) {
-    
-  }
+};
 
-});
+
+export default renderView;
